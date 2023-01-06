ac8364f7b167778c28941c69423d15e69c9acf74
Vinay <vinay143@gmail.com>
2023-01-06 01:26:59 +0000

    multipleValue to export5

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,12 @@
+let myArray = ["camel", 265, true, "5.6"];
+
+let bulb = {
+  watts: 10,
+  type: "LED",
+};
+
+let multiplyByFour = (num) => {
+  return 4 * num;
+};
+
+export { myArray, bulb, multiplyByFour };
